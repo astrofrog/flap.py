69c0c0280a8d8e1074c8d064692189fcb8a2570a
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-09-18 22:45:19 +0200

    Added settings

---
+++ settings.py
@@ -0,0 +1,29 @@
+window_width = 144
+window_height = 256
+scale = 2
+
+
+# Space under bricks
+empty_height = 300
+
+# Brick dimensions
+brick_width = 100
+brick_height = 18
+
+# Number of bricks
+nx = 7
+ny = 12
+
+# Margin between bricks
+margin = 5
+
+# Paddle dimensions
+paddle_width = 130
+paddle_height = 17
+
+# Ball radius
+ball_radius = 12
+
+# Colormap
+import matplotlib.pyplot as plt
+cmap = plt.cm.jet
