11e2e680fb73979020328f163c76c7271ea58fb3
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-09-18 21:14:13 +0200

    Added bird sprite

---
+++ bird.py
@@ -0,0 +1,56 @@
+import time
+from utils import get_sprite
+
+
+class Bird(object):
+    """
+    The flappy bird!
+    """
+
+    def __init__(self, scale, x_start, y_start):
+
+        # Load bird in different states
+
+        self.sprite_lib = [get_sprite('sprites/bird1.png', scale=scale),
+                           get_sprite('sprites/bird2.png', scale=scale),
+                           get_sprite('sprites/bird3.png', scale=scale)]
+        self.sprite_lib.append(self.sprite_lib[1])
+
+        self.curr_id = 1
+
+        self.flap_dt = 0.1
+        self.flap_t = 0
+
+        self.x = x_start
+        self.y = y_start
+        self.vy = 0
+        self.ay = -1000
+
+        self.dx = self.sprite.width
+        self.dy = self.sprite.height
+
+    @property
+    def sprite(self):
+        return self.sprite_lib[self.curr_id % 4]
+
+    def flap(self):
+        self.vy = 300
+
+    def update(self, dt):
+
+        # Flapping
+
+        self.flap_t += dt
+
+        if self.flap_t > self.flap_dt:
+            self.curr_id += 1
+            self.flap_t = 0
+
+        # Physics
+
+        self.vy += dt * self.ay
+        self.y += self.vy * dt
+
+    def blit(self):
+        self.sprite.blit(self.x - 0.5 * self.dx,
+                         self.y - 0.5 * self.dy)
